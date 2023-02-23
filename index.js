8a05591f32a865689c3d15a319e89c48094e4346
ankit8895 <125978383+ankit8895@users.noreply.github.com>
2023-02-23 15:40:30 +0530

    setup view engine

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const port = 8000;
 const app = express();
 
 
-//use express router
 
-app.use('/',require('./routes/index'));
+//use express router
+// app.use('/',require('./routes/index'));
+app.use('/',require('./routes'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err){
